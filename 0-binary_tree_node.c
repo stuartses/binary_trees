ca850418a072714039b58cc71f493174afcde4ae
stuartses <stuart.ses@hotmail.com>
2020-03-10 14:18:56 -0500

    Initializes right and left in NULL value

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -20,6 +20,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new_tree->n = value;
 	new_tree->parent = parent;
+	new_tree->left = NULL;
+	new_tree->right = NULL;
 
 	return (new_tree);
 }
