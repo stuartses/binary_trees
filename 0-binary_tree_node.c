d65f04e731cbb55e6af19ea89a8498bbefa48b33
stuartses <stuart.ses@hotmail.com>
2020-03-10 11:50:21 -0500

    Update when Malloc fails

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,6 +15,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_tree;
 
 	new_tree = malloc(sizeof(binary_tree_t));
+	if (new_tree == NULL)
+		return (NULL);
+
 	new_tree->n = value;
 	new_tree->parent = parent;
 
