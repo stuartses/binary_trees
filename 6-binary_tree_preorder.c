d3eeb061060caf9b17a7e1443100ad9d7dd18468
stuartses <stuart.ses@hotmail.com>
2020-03-11 16:06:15 -0500

    Implements Pre-order traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Pre-order traversal
+ * @tree: input node tree
+ * @func: Reference to a function with "n" as argument
+ *
+ * Description: Through a binary tree using pre-order traversal
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+
+	func(tree->n);
+
+	if (tree->left != NULL)
+		binary_tree_preorder(tree->left, *func);
+
+	if (tree->right != NULL)
+		binary_tree_preorder(tree->right, *func);
+}
