9868f21959ddc5eb084bf0d4d35b4a422fdd38e2
stuartses <stuart.ses@hotmail.com>
2020-03-12 10:35:36 -0500

    Insert Height tree function in the same file, and introduce condition when is a leave

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,48 @@
 #include "binary_trees.h"
 
 /**
+ * binary_height_bf - Binary tree height
+ * @tree: input node tree
+ *
+ * Description: Function that measures the height of a binary tree
+ * Return: height of tree
+ */
+size_t binary_height_bf(const binary_tree_t *tree)
+{
+	size_t count_left = 0;
+	size_t count_right = 0;
+	size_t height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	if (tree->left != NULL)
+	{
+		count_left += binary_height_bf(tree->left);
+		count_left++;
+	}
+
+	if (tree->right != NULL)
+	{
+		count_right += binary_height_bf(tree->right);
+		count_right++;
+	}
+
+	if (count_left > count_right)
+		height = count_left;
+	else if (count_right > count_left)
+		height = count_right;
+	else
+		height = count_left;
+
+	return (height);
+}
+
+
+/**
  * binary_tree_balance - balance factor of a binary tree
  * @tree: input tree
  *
@@ -15,15 +57,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (binary_tree_leaves(tree->left) == 1)
-		hleft = 1;
-	else
-		hleft = binary_tree_height(tree->left);
-
-	if (binary_tree_leaves(tree->right) == 1)
-		hright = 1;
-	else
-		hright = binary_tree_height(tree->right);
+	hleft = binary_height_bf(tree->left);
+	hright = binary_height_bf(tree->right);
 
 	bf = hleft - hright;
 
